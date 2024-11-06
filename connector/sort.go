d3f4194debc003ed311a38ae9daae9f145239ac7
m-Bilal <mohd.bilal1996@gmail.com>
2024-11-06 17:03:26 +0530

    support subtype for sort query ENT-124

--- connector/sort.go
+++ connector/sort.go
@@ -1,16 +1,18 @@
 package connector
 
 import (
+	"fmt"
+
 	"github.com/hasura/ndc-elasticsearch/internal"
 	"github.com/hasura/ndc-elasticsearch/types"
 	"github.com/hasura/ndc-sdk-go/schema"
 )
 
 // prepareSortQuery prepares the sort query.
-func prepareSortQuery(orderBy *schema.OrderBy, state *types.State, collection string) ([]map[string]interface{}, error) {
+func prepareSortQuery(orderBy *schema.OrderBy, state *types.State, collection string, configuration *types.Configuration) ([]map[string]interface{}, error) {
 	sort := make([]map[string]interface{}, len(orderBy.Elements))
 	for i, element := range orderBy.Elements {
-		sortElmnt, err := prepareSortElement(&element, state, collection)
+		sortElmnt, err := prepareSortElement(&element, state, collection, configuration)
 		if err != nil {
 			return nil, err
 		}
@@ -23,19 +25,36 @@ func prepareSortQuery(orderBy *schema.OrderBy, state *types.State, collection st
 //
 // It takes in the OrderByElement, state, and collection as parameters.
 // It returns the prepared sort element and an error if any.
-func prepareSortElement(element *schema.OrderByElement, state *types.State, collection string) (map[string]interface{}, error) {
+func prepareSortElement(element *schema.OrderByElement, state *types.State, collection string, configuration *types.Configuration) (map[string]interface{}, error) {
 	sort := make(map[string]interface{})
 	switch target := element.Target.Interface().(type) {
 	case *schema.OrderByColumn:
 		// Join the field path to get the field path and nested path.
 		fieldPath, nestedPath := joinFieldPath(state, target.FieldPath, target.Name, collection)
 
-		validField := internal.ValidateFieldOperation(state, "sort", collection, fieldPath)
+		validField := internal.ValidateSortOperation(state.SupportedSortFields, collection, fieldPath)
 		if validField == "" {
 			return nil, schema.UnprocessableContentError("sorting not supported on this field", map[string]any{
 				"value": fieldPath,
 			})
 		}
+
+		fieldType, fieldSubTypes, fieldDataEnabled, err := configuration.GetFieldProperties(collection, fieldPath)
+		if err != nil {
+			return nil, schema.InternalServerError("failed to get field types", map[string]any{"error": err.Error()})
+		}
+
+		if internal.IsSortSupported(fieldType, fieldDataEnabled) {
+			fieldPath = validField
+		} else {
+			for _, subType := range fieldSubTypes {
+				if internal.IsSortSupported(subType, fieldDataEnabled) {
+					validField = fmt.Sprintf("%s.%s", validField, subType)
+					break
+				}
+			}
+		}
+
 		fieldPath = validField
 		sort[fieldPath] = map[string]interface{}{
 			"order": string(element.OrderDirection),
