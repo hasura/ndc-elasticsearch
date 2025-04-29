4cc66c9ee90c061f866e9395b87116419d99950f
m-Bilal <mohd.bilal1996@gmail.com>
2025-04-29 19:19:05 +0530

    tests

--- connector/query_test.go
+++ connector/query_test.go
@@ -157,6 +157,22 @@ var tests = []test{
 		group: "flights_nested_flattened",
 		name: "nested_filtering_or",
 	},
+	{
+		group: "flights_nested",
+		name: "nested_filtering",
+	},
+	{
+		group: "flights_nested",
+		name: "nested_filtering_and",
+	},
+	{
+		group: "flights_nested",
+		name: "nested_filtering_range",
+	},
+	{
+		group: "flights_nested",
+		name: "nested_filtering_or",
+	},
 }
 
 func TestPrepareElasticsearchQuery(t *testing.T) {
